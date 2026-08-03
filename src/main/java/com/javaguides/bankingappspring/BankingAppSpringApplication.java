81f75e56711cb5bd92696f5e3a7504c09e7376f6
Krithika <krithisri19@gmail.com>
2026-08-03 20:19:18 +0530

    Initial Banking-app Project Setup

---
+++ src/main/java/com/javaguides/bankingappspring/BankingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.javaguides.bankingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javaguides/bankingappspring/BankingAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.bankingappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
