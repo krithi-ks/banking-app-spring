789db3fb098918d7bdc2d5e4ad06d355f6b54f4e
Krithika <krithisri19@gmail.com>
2026-08-04 00:27:45 +0530

    Add Withdraw REST API

--- src/main/java/com/javaguides/bankingappspring/service/AccountService.java
+++ src/main/java/com/javaguides/bankingappspring/service/AccountService.java
@@ -11,4 +11,6 @@ public interface AccountService {
     AccountDto getAccountById(Long id);
 
     AccountDto deposit(Long id, double amount);
+
+    AccountDto withdraw(Long id, double amount);
 }
--- src/main/java/com/javaguides/bankingappspring/service/impl/AccountServiceImpl.java
+++ src/main/java/com/javaguides/bankingappspring/service/impl/AccountServiceImpl.java
@@ -53,4 +53,22 @@ public class AccountServiceImpl implements AccountService {
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
     }
+
+    @Override
+    public AccountDto withdraw(Long id, double amount) {
+
+        Account account = accountRepository
+                .findById(id)
+                .orElseThrow(() -> new RuntimeException("Account does not exists"));
+
+        if(account.getBalance() < amount){
+            throw new RuntimeException("Insufficient amount");
+        }
+
+        double total = account.getBalance() - amount;
+        account.setBalance(total);
+        Account savedAccount = accountRepository.save(account);
+
+        return AccountMapper.mapToAccountDto(savedAccount);
+    }
 }
