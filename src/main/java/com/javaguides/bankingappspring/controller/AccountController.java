9112b1fe9a4698ee3263f8720ba30a1081f7b86b
Krithika <krithisri19@gmail.com>
2026-08-04 00:13:34 +0530

    Add Deposit REST API (PUT)

--- src/main/java/com/javaguides/bankingappspring/controller/AccountController.java
+++ src/main/java/com/javaguides/bankingappspring/controller/AccountController.java
@@ -2,10 +2,13 @@ package com.javaguides.bankingappspring.controller;
 
 
 import com.javaguides.bankingappspring.dto.AccountDto;
+import com.javaguides.bankingappspring.entity.Account;
 import com.javaguides.bankingappspring.service.AccountService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/accounts") //base url
@@ -30,5 +33,16 @@ public class AccountController {
     public ResponseEntity<AccountDto> getAccountById(@PathVariable Long id){
         AccountDto accountDto = accountService.getAccountById(id);
         return ResponseEntity.ok(accountDto);  // ok method returns http method
+    }
+
+    // Deposit REST API
+    @PutMapping("/{id}/deposit")
+    public ResponseEntity<AccountDto> deposit(@PathVariable Long id,
+                                              @RequestBody Map<String, Double> request){
+
+        Double amount = request.get("amount");
+
+        AccountDto accountDto = accountService.deposit(id, request.get("amount"));
+        return ResponseEntity.ok(accountDto);
     }
 }
